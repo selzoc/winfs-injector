a704f469b5ae4e58ca407914520204f344059d70
Dave Walter <dwalter@pivotal.io>
2017-11-07 10:52:58 -0800

    Checkpoint: known working release hydration
    [#152220456]
    
    Signed-off-by: Kalai Wei <kawei@pivotal.io>

--- main.go
+++ main.go
@@ -4,19 +4,23 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path"
+	"path/filepath"
+	"strings"
 
+	createRelease "github.com/cloudfoundry-incubator/windows2016fs-release/src/create/createRelease"
 	"github.com/pivotal-cf/jhanda/flags"
 	"github.com/pivotal-cf/winfs-injector/injector"
 )
 
 func main() {
-
-	var global struct {
-		InputTile string `short:"i" long:"input-tile"        description:"path to the input tile"                        default:""`
-		OutputDir string `short:"o" long:"output"            description:"path to extract to"                            default:""`
+	var arguments struct {
+		InputTile  string `short:"i" long:"input-tile"       description:"path to the input tile"                        default:""`
+		WorkingDir string `short:"w" long:"working-dir"          description:"tmp path to work in"                           default:""`
+		// Output     string `short:"o" long:"output"           description:"path to extract to"                            default:"."`
 	}
 
-	_, err := flags.Parse(&global, os.Args[1:])
+	_, err := flags.Parse(&arguments, os.Args[1:])
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -24,12 +28,27 @@ func main() {
 	var container = new(injector.ProductionExtractContainer)
 	var extractor = injector.NewExtractor(container)
 
-	destDir, err := extractor.ExtractWindowsFSRelease(global.InputTile, global.OutputDir)
+	destDir, err := extractor.ExtractWindowsFSRelease(arguments.InputTile, arguments.WorkingDir)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	fmt.Println(destDir)
+
+	imageName := "cloudfoundry/windows2016fs"
+
+	tileName := path.Base(arguments.InputTile)
+	tileName = strings.Replace(tileName, path.Ext(tileName), "", 1)
+
+	releaseDir := filepath.Join(destDir, tileName, "embed", "windows2016fs-release")
+
+	imageTagPath := filepath.Join(releaseDir, "src", "code.cloudfoundry.org", "windows2016fs", "IMAGE_TAG")
+
+	versionDataPath := filepath.Join(releaseDir, "VERSION")
+	var outputDir string
+	outputDir = filepath.Join(releaseDir, "blobs", "windows2016fs")
+	tarballPath := filepath.Join(outputDir, "windows2016fs-0.0.26.tgz")
+	createRelease.CreateRelease(imageName, releaseDir, tarballPath, imageTagPath, versionDataPath, outputDir)
 
 	// pathToScript := filepath.Join(destDir, "some-tile", "embed", "windows2016fs-release", "scripts", "create-release")
 	// fmt.Println(pathToScript)
