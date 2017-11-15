1776897d65b6cc6bf537de68f6f0e4838c81f7c6
Kevin Kelani <kkelani@pivotal.io>
2017-11-14 17:48:56 -0800

    adds a simple accetance test
    * none of the existing tests make sure the program compiles

---
+++ acceptance/sanity_check_test.go
@@ -0,0 +1,12 @@
+package acceptance
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("winfs-injector", func() {
+	It("compiles", func() {
+		Expect(true).To(BeTrue())
+	})
+})
