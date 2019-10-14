c0281e4ec1af04b0637e671a42bc723bb88840f4
Yael Harel <yharel@pivotal.io>
2019-10-14 10:56:13 -0400

    Acceptance test for required tile flags.
    [#168355122]
    
    Signed-off-by: Genevieve LEsperance <glesperance@pivotal.io>

---
+++ acceptance/main_test.go
@@ -0,0 +1,50 @@
+package acceptance_test
+
+import (
+	"os"
+	"os/exec"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/pivotal-cf/p-windows-runtime-2016/windowsfs-release/src/github.com/onsi/gomega/gexec"
+)
+
+var _ = Describe("acceptance", func() {
+	Describe("main", func() {
+		var (
+			winfsInjector string
+			cmd           *exec.Cmd
+			inputTile     string
+			outputTile    string
+		)
+		BeforeEach(func() {
+			var err error
+			winfsInjector, err = gexec.Build("github.com/pivotal-cf/winfs-injector")
+			Expect(err).ToNot(HaveOccurred())
+
+			inputTile = "input-tile-path"
+			outputTile = "output-tile-path"
+		})
+
+		AfterEach(func() {
+			Expect(os.Remove(winfsInjector)).NotTo(HaveOccurred())
+		})
+
+		It("requires an input tile path", func() {
+			cmd = exec.Command(winfsInjector, "-o", outputTile)
+			session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+			Expect(err).NotTo(HaveOccurred())
+			Eventually(session).Should(gexec.Exit(1))
+			Expect(string(session.Err.Contents())).To(ContainSubstring("--input-tile is required"))
+		})
+
+		It("requires an output tile path", func() {
+			cmd = exec.Command(winfsInjector, "-i", inputTile)
+			session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+			Expect(err).NotTo(HaveOccurred())
+			Eventually(session).Should(gexec.Exit(1))
+			Expect(string(session.Err.Contents())).To(ContainSubstring("--output-tile is required"))
+		})
+
+	})
+})
